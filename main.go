b7c315c9d99f79ef6c4e07a1425d62afc2e0b11e
keigooba <keigo2356@icloud.com>
2021-05-28 10:30:52 +0900

    git.sh実行

--- main.go
+++ main.go
@@ -1,11 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"snsall/app/controllers"
+	"net/http"
+	"os"
+	"os/exec"
 )
 
 func main() {
-	CmdFlag()
-	log.Println(controllers.StartWebServer())
+
+	http.HandleFunc("/", index)
+	http.HandleFunc("/version", versions)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defauting to port %s", port)
+	}
+
+	log.Printf("Linstening on port %s", port)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func index(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprint(w, "hello cloud run")
+}
+
+func versions(w http.ResponseWriter, _ *http.Request) {
+	s, err := exec.Command("gcloud", "version").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintf(w, string(s))
 }
