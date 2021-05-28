1197a9f6a30016feff771e43176be51ab5e128a2
keigooba <keigo2356@icloud.com>
2021-05-28 10:46:31 +0900

    git.sh実行

--- main.go
+++ main.go
@@ -1,30 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
-	"os"
+	"snsall/app/controllers"
 )
 
 func main() {
-
-	http.HandleFunc("/", index)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defauting to port %s", port)
-	}
-
-	log.Printf("Linstening on port %s", port)
-
-	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func index(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprint(w, "hello cloud run")
+	CmdFlag()
+	log.Println(controllers.StartWebServer())
 }
