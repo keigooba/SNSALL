943b2d2e9f3591db01305b41e326c802b87e8a00
keigooba <keigo2356@icloud.com>
2021-05-28 10:25:18 +0900

    git.sh実行

--- app/models/base.go
+++ app/models/base.go
@@ -1,30 +1,26 @@
 package models
 
 import (
-	"log"
-	"os"
-
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-func init() {
-	DBMS := "mysql"
-	USER := "root"
-	PASS := "root"
-	dbIp := os.Getenv("DB_IP")
-	var PROTOCOL string
-	if dbIp != "" {
-		PROTOCOL = "tcp(" + dbIp + ":3306)"
-	} else {
-		PROTOCOL = "tcp(127.0.0.1:3306)"
-	}
-	DBNAME := "gomix_db"
+// func init() {
+// 	DBMS := "mysql"
+// 	USER := "root"
+// 	PASS := "root"
+// 	dbIp := os.Getenv("DB_IP")
+// 	var PROTOCOL string
+// 	if dbIp != "" {
+// 		PROTOCOL = "tcp(" + dbIp + ":3306)"
+// 	} else {
+// 		PROTOCOL = "tcp(127.0.0.1:3306)"
+// 	}
+// 	DBNAME := "gomix_db"
 
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true"
-	_, err := gorm.Open(DBMS, CONNECT)
+// 	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true"
+// 	_, err := gorm.Open(DBMS, CONNECT)
 
-	if err != nil {
-		log.Fatal(err)
-	}
-}
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
